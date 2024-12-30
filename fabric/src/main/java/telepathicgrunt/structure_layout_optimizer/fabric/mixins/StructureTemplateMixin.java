a8a11474a39e25f1954183004d28b907da92f198
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-12-29 20:08:02 -0500

    temporary push for debugging help

--- fabric/src/main/java/telepathicgrunt/structure_layout_optimizer/fabric/mixins/StructureTemplateMixin.java
+++ fabric/src/main/java/telepathicgrunt/structure_layout_optimizer/fabric/mixins/StructureTemplateMixin.java
@@ -2,6 +2,7 @@ package telepathicgrunt.structure_layout_optimizer.fabric.mixins;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,8 +15,12 @@ import java.util.List;
 @Mixin(value = StructureTemplate.class)
 public class StructureTemplateMixin {
 
-    @Redirect(method = "placeInWorld(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructurePlaceSettings;Ljava/util/Random;I)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructureTemplate$Palette;blocks()Ljava/util/List;"))
-    private List<StructureTemplate.StructureBlockInfo> structureLayoutOptimizer$shrinkStructureTemplateBlocksList(StructureTemplate.Palette instance, @Local(ordinal = 0, argsOnly = true) BlockPos offset, @Local(ordinal = 0, argsOnly = true) StructurePlaceSettings settings) {
-        return StructureTemplateOptimizer.getStructureBlockInfosInBounds(instance, offset, settings);
+    @Redirect(method = "placeInWorld(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructurePlaceSettings;Ljava/util/Random;I)Z",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructureTemplate$Palette;blocks()Ljava/util/List;"))
+    private List<StructureTemplate.StructureBlockInfo> structureLayoutOptimizer$shrinkStructureTemplateBlocksList(StructureTemplate.Palette palette, @Local(ordinal = 0, argsOnly = true) ServerLevelAccessor serverLevel, @Local(ordinal = 0, argsOnly = true) BlockPos offset, @Local(ordinal = 1, argsOnly = true) BlockPos offset2, @Local(ordinal = 0, argsOnly = true) StructurePlaceSettings settings) {
+
+        settings.getProcessors().clear(); // clear processors for testing. No filtering will happen on air as a result to better see issues
+        return StructureTemplateOptimizer.getStructureBlockInfosInBounds(serverLevel, offset, offset2, settings, palette.blocks());
     }
+
 }
--- fabric/src/main/java/telepathicgrunt/structure_layout_optimizer/fabric/utils/StructureTemplateOptimizer.java
+++ fabric/src/main/java/telepathicgrunt/structure_layout_optimizer/fabric/utils/StructureTemplateOptimizer.java
@@ -1,6 +1,7 @@
 package telepathicgrunt.structure_layout_optimizer.fabric.utils;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.levelgen.structure.BoundingBox;
@@ -11,32 +12,91 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
+import static net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate.calculateRelativePosition;
+import static net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate.processBlockInfos;
+
 public class StructureTemplateOptimizer {
 
-    public static @NotNull List<StructureTemplate.StructureBlockInfo> getStructureBlockInfosInBounds(StructureTemplate.Palette instance, BlockPos offset, StructurePlaceSettings structurePlaceSettings) {
+    public static @NotNull List<StructureTemplate.StructureBlockInfo> getStructureBlockInfosInBounds(
+            LevelAccessor level,
+            BlockPos offset,
+            BlockPos offset2,
+            StructurePlaceSettings structurePlaceSettings,
+            List<StructureTemplate.StructureBlockInfo> originalPositions)
+    {
+
         BoundingBox boundingBox = structurePlaceSettings.getBoundingBox();
         if (boundingBox == null) {
-            return instance.blocks();
+            return originalPositions;
         }
 
         Mirror mirror = structurePlaceSettings.getMirror();
         Rotation rotation = structurePlaceSettings.getRotation();
         BlockPos pivot = structurePlaceSettings.getRotationPivot();
 
-        List<StructureTemplate.StructureBlockInfo> list = new ArrayList<>();
-        BlockPos.MutableBlockPos mutableBlockPos = new BlockPos.MutableBlockPos();
+        List<StructureTemplate.StructureBlockInfo> listOfInBoundsRelativePositions = new ArrayList<>();
+        List<StructureTemplate.StructureBlockInfo> listOfWorldPositions = new ArrayList<>();
+        BlockPos.MutableBlockPos mutableWorldPosition = new BlockPos.MutableBlockPos();
 
-        for (StructureTemplate.StructureBlockInfo blockInfo : instance.blocks()) {
-            mutableBlockPos.set(blockInfo.pos);
-            transform(mutableBlockPos, mirror, rotation, pivot);
-            mutableBlockPos.move(offset);
+        for (StructureTemplate.StructureBlockInfo relativeBlockInfo : originalPositions) {
 
-            if (boundingBox.isInside(mutableBlockPos)) {
-                list.add(blockInfo);
+            // vanilla position transforms
+            BlockPos test = calculateRelativePosition(structurePlaceSettings, relativeBlockInfo.pos).offset(offset);
+
+            // my mutable position transforms
+            mutableWorldPosition.set(relativeBlockInfo.pos);
+            transform(mutableWorldPosition, mirror, rotation, pivot);
+            mutableWorldPosition.move(offset);
+
+            // verify the transforms are correct
+            if (test.getX() != mutableWorldPosition.getX() || test.getY() != mutableWorldPosition.getY() || test.getZ() != mutableWorldPosition.getZ()) {
+                int breakpointHereNeverTriggers = 5;
+            }
+
+            StructureTemplate.StructureBlockInfo worldBlockInfo = new StructureTemplate.StructureBlockInfo(
+                    mutableWorldPosition.immutable(), relativeBlockInfo.state, relativeBlockInfo.nbt != null ? relativeBlockInfo.nbt.copy() : null
+            );
+
+            listOfWorldPositions.add(worldBlockInfo);
+
+            // Using the world positions for the bounds check. Same as line 229 in StructureTemplate's placeInWorld method (yarn)
+            // World position is same as vanilla's processBlockInfos method result.
+            if (structurePlaceSettings.getBoundingBox().isInside(worldBlockInfo.pos)) {
+                listOfInBoundsRelativePositions.add(relativeBlockInfo);
             }
         }
 
-        return list;
+        List<StructureTemplate.StructureBlockInfo> vanillaWorldBlockInfos = processBlockInfos(level, offset, offset2, structurePlaceSettings, originalPositions);
+
+        // Proves again that listOfWorldPositions is EXACTLY the same as processBlockInfos result down to the world position being used for bounds check.
+        List<StructureTemplate.StructureBlockInfo> worldDiffListFromVanilla = new ArrayList<>(vanillaWorldBlockInfos);
+        for (StructureTemplate.StructureBlockInfo worldVanillaBlockInfo : vanillaWorldBlockInfos) {
+            for (StructureTemplate.StructureBlockInfo worldBlockInfo : listOfWorldPositions) {
+                if (worldVanillaBlockInfo.pos.equals(worldBlockInfo.pos)) {
+                    worldDiffListFromVanilla.remove(worldVanillaBlockInfo);
+                }
+            }
+        }
+
+        // The check to verify that yes, we transformed the positions correctly to match vanilla exactly.
+        if (!worldDiffListFromVanilla.isEmpty()) {
+            int breakpointHereNeverTriggers = 5;
+        }
+
+        List<StructureTemplate.StructureBlockInfo> vanillaFilteredWorldBlockInfos = new ArrayList<>();
+        for (StructureTemplate.StructureBlockInfo worldVanillaBlockInfo : vanillaWorldBlockInfos) {
+            if (structurePlaceSettings.getBoundingBox().isInside(worldVanillaBlockInfo.pos)) {
+                vanillaFilteredWorldBlockInfos.add(worldVanillaBlockInfo);
+            }
+        }
+
+        // Check to make sure that vanilla's filtered result will be the same size list. Proves we filtered the correct number of positions.
+        if (vanillaFilteredWorldBlockInfos.size() != listOfInBoundsRelativePositions.size()) {
+            int breakpointHereNeverTriggers = 5;
+        }
+
+        // Using seed -4673899320199289746, go to village here to see missing center well: /tp @s -432 ~ -144
+        return listOfInBoundsRelativePositions;
     }
 
     private static void transform(BlockPos.MutableBlockPos mutableBlockPos, Mirror mirror, Rotation rotation, BlockPos pivot) {
