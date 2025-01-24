4e460cc68b483814911cf00a13e152262514831a
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2025-01-24 13:39:26 -0500

    fixed fabric and forge

--- forge/src/main/java/telepathicgrunt/structure_layout_optimizer/forge/entrypoints/Main.java
+++ forge/src/main/java/telepathicgrunt/structure_layout_optimizer/forge/entrypoints/Main.java
@@ -1,10 +1,6 @@
 package telepathicgrunt.structure_layout_optimizer.forge.entrypoints;
 
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.fml.loading.FMLPaths;
 import telepathicgrunt.structure_layout_optimizer.StructureLayoutOptimizerMod;
 import telepathicgrunt.structure_layout_optimizer.configs.SLOConfig;
@@ -12,7 +8,7 @@ import telepathicgrunt.structure_layout_optimizer.configs.SLOConfig;
 @Mod(StructureLayoutOptimizerMod.MODID)
 public class Main {
 
-    public Main(FMLJavaModLoadingContext context) {
+    public Main() {
         StructureLayoutOptimizerMod.init();
         SLOConfig.configStartup(FMLPaths.CONFIGDIR.get());
     }
