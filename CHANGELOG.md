bd3526d5a9f9aad79a8ca7a1998af03ecd1e8c21
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2025-10-04 14:55:00 -0400

    trying to fix mixin but no luck

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,2 @@
 ## v1.1.2
-Fixed crash in 1.21.9
+Fixed crash in 1.21.9 Fabric
--- common/src/main/java/telepathicgrunt/structure_layout_optimizer/mixins/JigsawPlacementMixin.java
+++ common/src/main/java/telepathicgrunt/structure_layout_optimizer/mixins/JigsawPlacementMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.world.level.levelgen.structure.pools.JigsawPlacement;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import telepathicgrunt.structure_layout_optimizer.utils.BoxOctree;
@@ -17,7 +18,9 @@ import telepathicgrunt.structure_layout_optimizer.utils.TrojanVoxelShape;
 public class JigsawPlacementMixin {
 
     // need the intermediary name of the lambda method because fabric remaps those and uses intermediary even in dev
-    @WrapOperation(method = {"lambda$addPieces$2", "method_39824"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/phys/shapes/Shapes;join(Lnet/minecraft/world/phys/shapes/VoxelShape;Lnet/minecraft/world/phys/shapes/VoxelShape;Lnet/minecraft/world/phys/shapes/BooleanOp;)Lnet/minecraft/world/phys/shapes/VoxelShape;"), require = 1)
+    @WrapOperation(method = {"lambda$addPieces$2", "Lnet/minecraft/structure/pool/StructurePoolBasedGenerator;method_39824(Lnet/minecraft/structure/PoolStructurePiece;IILnet/minecraft/world/gen/structure/JigsawStructure$MaxDistanceFromCenter;ILnet/minecraft/world/HeightLimitView;Lnet/minecraft/world/gen/structure/DimensionPadding;ILnet/minecraft/util/math/BlockBox;Lnet/minecraft/world/gen/structure/Structure$Context;ZLnet/minecraft/world/gen/chunk/ChunkGenerator;Lnet/minecraft/structure/StructureTemplateManager;Lnet/minecraft/util/math/random/ChunkRandom;Lnet/minecraft/registry/Registry;Lnet/minecraft/structure/pool/alias/StructurePoolAliasLookup;Lnet/minecraft/structure/StructureLiquidSettings;Lnet/minecraft/structure/StructurePiecesCollector;)V"},
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/phys/shapes/Shapes;join(Lnet/minecraft/world/phys/shapes/VoxelShape;Lnet/minecraft/world/phys/shapes/VoxelShape;Lnet/minecraft/world/phys/shapes/BooleanOp;)Lnet/minecraft/world/phys/shapes/VoxelShape;"),
+            require = 1)
     private static VoxelShape structureLayoutOptimizer$replaceVoxelShape1(VoxelShape shape1, VoxelShape shape2, BooleanOp function, Operation<VoxelShape> original, @Local(ordinal = 0) AABB aabb, @Local(ordinal = 0, argsOnly = true) BoundingBox boundingbox) {
         TrojanVoxelShape trojanVoxelShape = new TrojanVoxelShape(new BoxOctree(aabb));
         trojanVoxelShape.boxOctree.addBox(AABB.of(boundingbox));
